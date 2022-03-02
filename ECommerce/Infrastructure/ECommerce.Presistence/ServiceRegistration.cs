6a2d740f4f535cbfe528c306fbb9451709131f07
caner0zkan <canerozkan513@gmail.com>
2022-03-02 18:00:49 +0300

    layers created

---
+++ ECommerce/Infrastructure/ECommerce.Presistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Presistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
